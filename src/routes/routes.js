f7634aae15fd1a796365228fccfa6825ce3b0916
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-04-27 09:55:35 +0300

    Set up API endpoints

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { Users, Doctors } from "../models/models.js";
+import generateTimeSlots from "../services/timestamp.js";
 
 const router = express.Router();
 
@@ -26,24 +27,86 @@ router.post('/users', async (req, res) => {
     await newUser.save();
     res.status(201).json(newUser);
   } catch (err) {
-    res.status(500).json({ message: 'Server error', error: err });
+    res.status(500).json({ message: 'Server error', error: err.message || err });
   }
+});
+
+router.get('/doctors', async (req, res) => {
+  Doctors.find()
+  .then((doctors) => {
+      res.status(201).json(doctors);
+  })
+  .catch((error) => {
+      res.status(500).send('Error ' + error);
+  })
 });
 
 router.post('/doctors', async (req, res) => {
   try {
-    const { name, spec, slots } = req.body;
+    const { name, spec } = req.body;
 
-    const existingDoctor = await Doctors.findOne({ id });
+    const existingDoctor = await Doctors.findOne({ name });
     if (existingDoctor) {
       return res.status(400).json({ message: 'Doctor already exists' });
     }
-
+    
+    const slots = generateTimeSlots(9, 17);
     const newDoctor = new Doctors({ name, spec, slots });
     await newDoctor.save();
     res.status(201).json(newDoctor);
   } catch (err) {
-    res.status(500).json({ message: 'Server error', error: err });
+    res.status(500).json({ message: 'Server error', error: err.message || err });
+  }
+});
+
+router.post('/appointments', async (req, res) => {
+  const { user_id, doctor_id, slot } = req.body;
+
+  try {
+    // Check if the user and doctor exist
+    const user = await Users.findById(user_id);
+    const doctor = await Doctors.findById(doctor_id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    if (!doctor) {
+      return res.status(404).json({ message: 'Doctor not found' });
+    }
+
+    // Check if the slot is available for the doctor
+    const slotAvailable = doctor.slots.includes(slot);
+    if (!slotAvailable) {
+      return res.status(400).json({ message: 'Slot not available' });
+    }
+
+    // Update the doctor's slots by removing the booked slot
+    doctor.slots = doctor.slots.filter(s => s !== slot);
+    await doctor.save();
+
+    // Update the user's appointments with the new appointment information
+    user.appointments.push({ doctor: doctor_id, slot });
+    await user.save();
+
+    const appointmentDate = new Date(slot);
+    const oneDayBefore = new Date(appointmentDate.getTime() - (24 * 60 * 60 * 1000)).toISOString();
+    const twoHoursBefore = new Date(appointmentDate.getTime() - (2 * 60 * 60 * 1000)).toISOString();
+
+    const newAppointment = new Appointment({
+      user: user_id,
+      doctor: doctor_id,
+      appointmentTime: slot,
+      notificationTime: {
+        oneDayBefore,
+        twoHoursBefore,
+      },
+    });
+
+    await newAppointment.save();
+    res.status(200).json({ message: 'Appointment booked successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err.message || err });
   }
 });
 
