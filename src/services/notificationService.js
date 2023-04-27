b225e2ce5c30a26368e1d6aef43874be75b2420e
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-04-28 00:49:41 +0300

    Implemented 24/2 hours appointment notifications

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -1,8 +1,10 @@
-import { Appointments } from "../models/models";
-import { fs } from "fs";
+import { Appointments } from "../models/models.js";
+import { getCurrentTimeInUTC3 } from "../controllers/controller.js";
+import path from "path";
+import fs from "fs";
 
 const logNotification = (message) => {
-    const logFile = 'notifications.log';
+    const logFile = "./src/logs/notifications.log";
     const logMessage = `${new Date().toISOString()} | ${message}\n`;
     fs.appendFile(logFile, logMessage, (err) => {
       if (err) {
@@ -10,25 +12,43 @@ const logNotification = (message) => {
       }
     });
 };
-  
+
 export const checkAppointments = async () => {
-    const currentTime = new Date().toISOString();
-  
-    const appointments = await Appointment.find({
-      $or: [
-        { 'notificationTime.oneDayBefore': currentTime },
-        { 'notificationTime.twoHoursBefore': currentTime },
-      ],
+    const currentTime = getCurrentTimeInUTC3().toISOString().slice(0, 16);
+    const expectedTime = `${currentTime}` + ':00.000Z'
+
+    console.log(`Started checking appointments for ${currentTime}`);
+
+    const appointments = await Appointments.find({
+        $or: [
+          { 'notificationTime.oneDayBefore': expectedTime },
+          { 'notificationTime.twoHoursBefore': expectedTime },
+        ],
     }).populate('user').populate('doctor');
-  
+
     appointments.forEach(appointment => {
-      const user = appointment.user;
-      const doctor = appointment.doctor;
-      const slot = appointment.appointmentTime;
-      const message = currentTime === appointment.notificationTime.oneDayBefore
-        ? `Привет ${user.name}! Напоминаем что вы записаны к ${doctor.spec} завтра в ${slot}!`
-        : `Привет ${user.name}! Вам через 2 часа к ${doctor.spec} в ${slot}!`;
-  
-      logNotification(message);
+
+        const user = appointment.user;
+        const doctor = appointment.doctor;
+        const slot = appointment.appointmentTime;
+        const dateTimeString  = expectedTime.split("T")[0];
+        const slotTime = slot.split("T")[1].substring(0, 5);
+
+        const notificationTimes = [
+            appointment.notificationTime.oneDayBefore,
+            appointment.notificationTime.twoHoursBefore
+        ];
+        console.log(notificationTimes);
+
+        if (expectedTime === appointment.notificationTime.oneDayBefore) {
+            const message = `${dateTimeString} | Привет, ${user.name}! Напоминаем что вы записаны к ${doctor.spec}у завтра в ${slotTime}!`;
+            logNotification(message);
+            console.log(message);
+          } else if (expectedTime === appointment.notificationTime.twoHoursBefore) {
+            const message = `${dateTimeString} | Привет, ${user.name}! Вам через 2 часа к ${doctor.spec}у в ${slotTime}!`;
+            logNotification(message);
+            console.log(message);
+        }
     });
-  };
+    console.log(`Finished checking appointments for ${currentTime}`);
+}
