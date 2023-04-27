dfb7f68c3bac5e6f724664874677afa4b53c72b5
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-04-27 09:54:18 +0300

    Added notification timeiut function

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/routes.js";
+import { checkAppointments } from "./services/notificationService.js";
 
 const app = express();
 const PORT = process.env.port || 3000;
+
 
 mongoose.connect('mongodb+srv://testing:KAuoRdRk3u5ZLFCe@medapi.iqw3a70.mongodb.net/MedApi?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -19,6 +21,9 @@ mongoose.connect('mongodb+srv://testing:KAuoRdRk3u5ZLFCe@medapi.iqw3a70.mongodb.
 app.use(express.json());
 app.use("/api", router);
 
+// Schedule the task to check appointments every minute
+const checkAppointmentsInterval = 60 * 1000;
+setInterval(checkAppointments, checkAppointmentsInterval);
 
 app.get('/', (req, res) => {
     res.send('Hello, this is a simple Medical API used to create appointments!');
