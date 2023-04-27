002c234be5984408a9854e74bab993f484922327
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-04-27 05:29:09 +0300

    Renamed imported mongoose models

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,28 @@
 import express from "express";
-import { UserModel, DoctorModel } from "../models/models.js";
+import { Users, Doctors } from "../models/models.js";
 
 const router = express.Router();
+
+router.get('/users', async (req, res) => {
+  Users.find()
+  .then((users) => {
+      res.status(201).json(users);
+  })
+  .catch((error) => {
+      res.status(500).send('Error ' + error);
+  })
+});
 
 router.post('/users', async (req, res) => {
   try {
     const { phone, name, appointments } = req.body;
 
-    const existingUser = await UserModel.findOne({ name });
+    const existingUser = await Users.findOne({ name });
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    const newUser = new UserModel({  phone, name, appointments });
+    const newUser = new Users({  phone, name, appointments });
     await newUser.save();
     res.status(201).json(newUser);
   } catch (err) {
@@ -24,12 +34,12 @@ router.post('/doctors', async (req, res) => {
   try {
     const { name, spec, slots } = req.body;
 
-    const existingDoctor = await DoctorModel.findOne({ id });
+    const existingDoctor = await Doctors.findOne({ id });
     if (existingDoctor) {
       return res.status(400).json({ message: 'Doctor already exists' });
     }
 
-    const newDoctor = new DoctorModel({ name, spec, slots });
+    const newDoctor = new Doctors({ name, spec, slots });
     await newDoctor.save();
     res.status(201).json(newDoctor);
   } catch (err) {
