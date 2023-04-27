56803215d01a652e1fabe100942c64a1e4e00d59
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-04-27 04:16:10 +0300

    Connected to mongoose

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
+import { UserModel, DoctorModel } from "./models/models.js";
 
 const app = express();
 const PORT = process.env.port || 3000;
+
+mongoose.connect('mongodb+srv://testing:KAuoRdRk3u5ZLFCe@medapi.iqw3a70.mongodb.net/MedApi?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Connected to MongoDB');
+  })
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello, this is a simple Medical API used to create appointments!');
